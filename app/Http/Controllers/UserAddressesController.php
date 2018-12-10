a5ace9f38e73fb546186f098477ec6a99bc19871
v5dxiaoyu <v5dxiaoyu@gmail.com>
2018-12-10 15:51:33 +0000

    modify and delete addresses

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -10,7 +10,10 @@ use App\Http\Requests\UserAddressRequest;
 class UserAddressesController extends Controller
 {
     //
-
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(Request $request)
     {
         return view('user_addresses.index', [
@@ -23,6 +26,10 @@ class UserAddressesController extends Controller
         return view('user_addresses.create_and_edit',['address'=>new UserAddress()]);
     }
 
+    /**
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(UserAddressRequest $request)
     {
         $request->user()->address()->create($request->only([
@@ -39,5 +46,51 @@ class UserAddressesController extends Controller
 
         return redirect()->route('user_addresses.index');
     }
+
+    /**
+     * @param UserAddress $user_address
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse、
+     */
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contract_name',
+            'contact_phone'
+        ]));
+        return redirect()->route('user_addresses.index');
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit',[
+            'address'=> $user_address
+        ]);
+    }
+
+    /**
+     * @param UserAddress $user_address
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Useraddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $update = $user_address;//此处需要修改为软删除
+
+        //return redirect()->route('user_addresses.index');
+        return [];
+    }
+
 
 }
